d2383bd78cfd8611e4fd03a31916657b9b4ceb57
SachHypixel <sacha@hypixelstudios.com>
2021-07-19 16:08:05 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using SFML.Graphics;
+using SFML.Window;
+using System;
+
+namespace FlappyBirdClone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RenderWindow window = new RenderWindow(new VideoMode(1024, 768), "SFML graphics with OpenGL", Styles.Default);
+            var cornFlowerBlue = new Color(154, 206, 235);
+
+            window.SetVerticalSyncEnabled(true);
+
+            window.Closed += (_, __) => window.Close();
+            window.KeyReleased += Window_KeyReleased;
+
+            while (window.IsOpen)
+            {
+                window.DispatchEvents();
+                window.Clear(cornFlowerBlue);
+                window.Display();
+            }
+        }
+
+        private static void Window_KeyReleased(object sender, KeyEventArgs e)
+        {
+            if (e.Code == Keyboard.Key.Space)
+            {
+
+            }
+        }
+    }
+}
